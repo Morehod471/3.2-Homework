4b4f2312e78300b9658a040037c295dbfa32225a
Homework2 <ivliev.k@yandex.ru>
2023-10-08 14:21:42 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class FacultyService {
+
+    private final Map<Long, Faculty> storage = new HashMap<>();
+    private long counter = 0;
+
+    public Faculty add(Faculty faculty) {
+        faculty.setId(counter);
+        storage.put(counter, faculty);
+        counter++;
+        return faculty;
+    }
+
+    public Faculty get(long id) {
+        return storage.get(id);
+    }
+
+    public boolean remove(long id) {
+        return storage.remove(id) != null;
+    }
+
+    public Faculty update(Faculty faculty) {
+        if (storage.containsKey(faculty.getId())) {
+            storage.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+
+    public Collection<Faculty> filterByColor(String color) {
+        return storage.values().stream()
+                .filter(faculty -> faculty.getColor().equals(color))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+
+    private final Map<Long, Student> storage = new HashMap<>();
+    private long counter = 0;
+
+    public Student add(Student student) {
+        student.setId(counter);
+        storage.put(counter, student);
+        counter++;
+        return student;
+    }
+
+    public Student get(long id) {
+        return storage.get(id);
+    }
+
+    public boolean remove(long id) {
+        return storage.remove(id) != null;
+    }
+
+    public Student update(Student student) {
+        if (storage.containsKey(student.getId())) {
+            storage.put(student.getId(), student);
+            return student;
+        }
+        return null;
+    }
+
+    public Collection<Student> filterByAge(int age) {
+        return storage.values().stream()
+                .filter(student -> student.getAge() == age)
+                .collect(Collectors.toList());
+    }
+}
