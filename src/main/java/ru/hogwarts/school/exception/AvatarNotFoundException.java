84ec719756d9b1e46fb28a2dcf450645293062aa
Homework2 <ivliev.k@yandex.ru>
2023-10-23 14:18:53 +0300

    Initial commit

--- src/main/java/ru/hogwarts/school/exception/AvatarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.hogwarts.school.exception;
-
-public class AvatarNotFoundException extends RuntimeException {
-
-    public AvatarNotFoundException(String message) {
-        super(message);
-    }
-
-}
