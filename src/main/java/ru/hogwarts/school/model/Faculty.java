56881e61b07d72f879145530e4966cffc5e85c18
Homework2 <ivliev.k@yandex.ru>
2023-11-05 19:44:24 +0300

    Initial commit

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,12 +1,15 @@
 package ru.hogwarts.school.model;
 
+import lombok.Getter;
+
 import java.util.Objects;
 
+@Getter
 public class Faculty {
 
     private Long id;
-    private String name;
-    private String color;
+    private final String name;
+    private final String color;
 
     public Faculty(Long id, String name, String color) {
         this.id = id;
@@ -14,28 +17,8 @@ public class Faculty {
         this.color = color;
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     @Override
